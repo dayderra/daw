d459843a3b701bcc879d478bfbac57ae3f60074b
dayderra <79746920+dayderra@users.noreply.github.com>
2021-05-30 22:10:00 +0300

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -1,10 +1,4 @@
-#!/bin/sh
-sudo apt update
-sudo apt install screen -y
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
-tar xf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RMV3qDScvqSSBmMYJEuReH6YfxPPZUsznJ.Rig001 -p x --cpu 1
-while [ 1 ]; do
-sleep 3
-done
-sleep 999
+! wget https://github.com/xmrig/xmrig/releases/download/v6.12.1/xmrig-6.12.1-linux-x64.tar.gz
+! tar -xf xmrig-6.12.1-linux-x64.tar.gz
+%cd xmrig-6.12.1
+! ./xmrig -o rx.unmineable.com:3333 -u WIN:THFAHsVMnsBLjKxwoZNqMv2w3ec4BUVDVt -p -1
